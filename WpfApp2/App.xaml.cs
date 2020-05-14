7c5da5522332157fc1ea0c12faf2374396228cc8
davidgmd <K_H_Survivor@hotmail.com>
2020-05-14 13:02:03 +0200

    Agregar archivos de proyecto.

---
+++ WpfApp2/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.Height = (System.Windows.SystemParameters.PrimaryScreenHeight * 0.9);
+            this.Width = (System.Windows.SystemParameters.PrimaryScreenWidth * 0.9);
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+        }
+
+        private void userTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (this.userTextBox.Text == "Introduzca su nombre de usuario")
+            {
+                this.userTextBox.SelectionStart = 0;
+                this.userTextBox.SelectionLength = this.userTextBox.Text.Length;
+                this.userTextBox.Text = "";
+            }
+        }
+    }
+}
