65cf6bb5e7bf851afb889431da2d4241c23f65b4
davidgmd <K_H_Survivor@hotmail.com>
2020-05-15 13:36:56 +0200

    Trabajando y añadiendo codigo del menu principal

--- WpfApp2/View/menuPrincipal.xaml.cs
+++ WpfApp2/View/menuPrincipal.xaml.cs
@@ -21,5 +21,33 @@ namespace WpfApp2.View
         {
             InitializeComponent();
         }
+
+        private void CampaigneComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.botonCalendario.IsEnabled = true;
+            this.botonOpciones.IsEnabled = true;
+            this.botonRecursos.IsEnabled = true;
+            this.botonTirador.IsEnabled = true;
+
+            var item = (ComboBoxItem)this.campaigneComboBox.SelectedValue;
+                var content = (TextBlock)item.Content;
+            
+
+            switch (content.Text.Trim())
+            {
+                case "D&D 3.5":
+                    
+                    this.iconoCampaigne.Source = new BitmapImage(new Uri("/images/icons/D&D.png", UriKind.Relative));
+                    break;
+
+                case "Warhammer":
+                    this.iconoCampaigne.Source = new BitmapImage(new Uri("/images/icons/warhammer.jpg", UriKind.Relative));
+                    break;
+
+                default:
+                    MessageBox.Show(content.Text);
+                    break;
+            }
+        }
     }
 }
