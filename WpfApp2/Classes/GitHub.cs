fd9c9948d543fb212ad64e3d1cc65598b7c45e67
David Pinedo Solano <k_h_survivor@hotmail.com>
2020-05-21 01:30:51 +0200

    Modificado github y conseguida autentificacion

--- WpfApp2/Classes/GitHub.cs
+++ WpfApp2/Classes/GitHub.cs
@@ -1,10 +1,18 @@
 ﻿using Octokit;
+using System.Threading.Tasks;
 
 namespace ElEscribaDelDJ.Classes
 {
     class GitHub
     {
         private GitHubClient cliente;
+        private Credentials credenciales;
+
+        public Credentials Credenciales
+        {
+            get { return credenciales; }
+            set { credenciales = value; }
+        }
 
         public GitHubClient Cliente
         {
@@ -15,7 +23,17 @@ namespace ElEscribaDelDJ.Classes
         public GitHub()
         {
             cliente = new GitHubClient(new ProductHeaderValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName));
-            cliente = cliente;
+            credenciales = new Credentials("aa7f94ddd461fb37d47e6db87527589a7a7529c4");
+            ObtenerDatos();
+            
+        }
+
+        public async Task ObtenerDatos()
+        {
+            var user = await this.cliente.User.Get("davidgmd");
+            var repositorio = cliente.Repository.Get("davidgmd", "Proyecto-de-fin-de-grado");
+            repositorio.
+            var cadena = user.Bio.ToString();
         }
     }
 }
