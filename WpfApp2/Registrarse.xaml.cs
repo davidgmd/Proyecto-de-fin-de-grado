ea9a3f41c983c2dc647b4f7b0edeb5a92f99cb51
davidgmd <K_H_Survivor@hotmail.com>
2020-05-24 14:41:09 +0200

    añadido detalles al registro y a los creditos

--- WpfApp2/Registrarse.xaml.cs
+++ WpfApp2/Registrarse.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,26 +19,69 @@ namespace ElEscribaDelDJ
     /// </summary>
     public partial class Registrarse : Window
     {
+        private List<Object> lista = new List<Object>();
+
         public Registrarse()
         {
             InitializeComponent();
+
+
         }
 
         private async void User_LostFocus(object sender, RoutedEventArgs e)
         {
-            
-            if (await MainWindow.gitHub.UsuarioExiste(this.userText.Text) == true)
+            if (this.userText.Text != "")
             {
-                ImgUser.Source = new BitmapImage(new Uri("/images/icons/icons8-verificado.png", UriKind.Relative));
-                ErrorUser.Text = "";
-                AceptarButton.IsEnabled = true;
-            }
-            else
+                if (await MainWindow.gitHub.UsuarioExiste(this.userText.Text) == false)
+                {
+                    this.MarcarCorrecto(ImgUser, ErrorUser, "Nombre de usuario disponible");
+                    lista.Add(ImgUser);
+                }
+                else
+                {
+                    this.MarcarIncorrecto(ImgUser, ErrorUser, "Ya existe alguien con ese usuario");
+                    lista.Remove(ImgUser);
+                }
+            }                     
+        }
+
+        private void userText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var regExp = new Regex("[a-zA-Z0-9]*$");
+            if (userText.Text == "")
             {
-                ImgUser.Source = new BitmapImage(new Uri("/images/icons/icons8-incorrecto.png", UriKind.Relative));
-                ErrorUser.Text = "Ese nombre de usuario ya existe";
-                AceptarButton.IsEnabled = false;
+                this.MarcarIncorrecto(ImgUser, ErrorUser, "Introduzca el nombre de usuario");
             }
+            else 
+            {
+                if (regExp.IsMatch("[a-zA-Z0-9]*$"))
+                {
+                    ImgUser.Visibility = Visibility.Collapsed;
+                    ErrorUser.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    this.MarcarIncorrecto(ImgUser, ErrorUser, "El nombre de usuario debe empezar por letras y no usar signos especiales");
+                }    
+            }           
+        }
+
+        private void MarcarCorrecto(Image elementoimagen, TextBlock elementotexto, string texto)
+        {
+            elementoimagen.Visibility = Visibility.Visible;
+            elementotexto.Visibility = Visibility.Visible;
+            elementoimagen.Source = new BitmapImage(new Uri("/images/icons/icons8-verificado.png", UriKind.Relative));
+            elementotexto.Text = texto;
+            elementotexto.Foreground = Brushes.Green;
+        }
+
+        private void MarcarIncorrecto(Image elementoimagen, TextBlock elementotexto, string texto)
+        {
+            elementoimagen.Visibility = Visibility.Visible;
+            elementotexto.Visibility = Visibility.Visible;
+            elementoimagen.Source = new BitmapImage(new Uri("/images/icons/icons8-incorrecto.png", UriKind.Relative));
+            elementotexto.Text = texto;
+            elementotexto.Foreground = Brushes.Red;
         }
     }
 }
