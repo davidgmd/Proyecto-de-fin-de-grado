0053abd67829e2d040979026995281b8c1159e0f
davidgmd <K_H_Survivor@hotmail.com>
2020-06-19 10:38:15 +0200

    Añadidas las opciones sin funcionalidad

---
+++ WpfApp2/View/Options/VentanaOpciones.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ElEscribaDelDJ.View.Options
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaOpciones.xaml
+    /// </summary>
+    public partial class VentanaOpciones : Window
+    {
+        public VentanaOpciones()
+        {
+            InitializeComponent();
+        }
+    }
+}
