dfa53e315d85d90cf579557305469d9174c7d094
David Pinedo Solano <k_h_survivor@hotmail.com>
2020-10-21 15:06:11 +0200

    Al cambiar el build action y el copiar directorio ahora en la carpeta del ejecutable se crean los archivos necesarios.

--- WpfApp2/ConfiguracionAplicacion.Designer.cs
+++ WpfApp2/ConfiguracionAplicacion.Designer.cs
@@ -12,7 +12,7 @@ namespace ElEscribaDelDJ {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class ConfiguracionAplicacion : global::System.Configuration.ApplicationSettingsBase {
         
         private static ConfiguracionAplicacion defaultInstance = ((ConfiguracionAplicacion)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new ConfiguracionAplicacion())));
