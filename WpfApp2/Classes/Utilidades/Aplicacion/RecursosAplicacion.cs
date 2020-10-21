5c39d51a4ad381ce4f7673f3d7679d90a68e95df
David Pinedo Solano <k_h_survivor@hotmail.com>
2020-10-21 22:06:34 +0200

    Se han ajustado todas las direcciones, para que esten en una colección clave, valor, sin embargo ahora toda referencia a una dirección fallara.

--- WpfApp2/Classes/Utilidades/Aplicacion/RecursosAplicacion.cs
+++ WpfApp2/Classes/Utilidades/Aplicacion/RecursosAplicacion.cs
@@ -9,27 +9,29 @@ namespace ElEscribaDelDJ.Classes.Utilidades.Aplicacion
 {
     public static class RecursosAplicacion
     {
-        private static string direccionbase = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\"));
-        private static Usuario sesionusuario;
-        private static JObject userjobject;
-        public static readonly string Idiomas = direccionbase + "\\Idiomas\\";
-        public static readonly string Recursos = direccionbase + "\\Resources\\";
-        public static readonly string Usuarios = direccionbase + "\\Usuarios\\";
-        public static readonly string ImagenIcono = direccionbase + "\\Images\\icons\\";
-        public static readonly string ImagenUsuario = direccionbase + $"\\Images\\User\\";
+        private static string direccionbase = System.IO.Path.GetDirectoryName(
+System.Reflection.Assembly.GetExecutingAssembly().Location);
+        private static Usuario _sesionusuario;
+        private static JObject _userjobject;
+        private static SortedList<string, string> _directoriosaplicacion = new SortedList<string, string>;
 
+        public static SortedList<string, string> Directorios
+        {
+            get { return _directoriosaplicacion; }
+            set { _directoriosaplicacion = value; }
+        }
 
         public static JObject UserJobject
         {
-            get { return userjobject; }
-            set { userjobject = value; }
+            get { return _userjobject; }
+            set { _userjobject = value; }
         }
 
 
         public static Usuario SesionUsuario
         {
-            get { return sesionusuario; }
-            set { sesionusuario = value; }
+            get { return _sesionusuario; }
+            set { _sesionusuario = value; }
         }
 
 
@@ -41,23 +43,26 @@ namespace ElEscribaDelDJ.Classes.Utilidades.Aplicacion
 
         public static void EntornoInicialAplicacion()
         {
-            //obtenemos la dirección del ejecutable
-            string direccionejecutable = System.IO.Path.GetDirectoryName(
-System.Reflection.Assembly.GetExecutingAssembly().Location);
-            //creamos varias carpetas iniciales, la primera es donde iran los logs, luego de utilidades, los archivos para encriptar y credenciales
-            //aparte de estos tenemos los de imagenes, idiomas, y uno para los datos de usuario
-            string directoriologs = direccionejecutable + "\\Logs\\";
-            Directory.CreateDirectory(directoriologs);
-            string directorioclaves = direccionejecutable + "\\Utils\\Claves";
-            string directoriocredenciales = direccionejecutable + "\\Utils\\Credenciales";
-            string directorioiconos = direccionejecutable + "\\Imagenes\\Icons";
-            string imagenespordefecto = direccionejecutable + "\\Imagenes\\Default";
-            string idiomaingles = direccionejecutable + "\\Idiomas\\EN\\";
-            string idiomaespanol = direccionejecutable + "\\Idiomas\\ES\\";
-            string imagenesusuario = direccionejecutable + "\\Usuarios\\Imagenes\\";
-            string archivossusuario = direccionejecutable + "\\Usuarios\\Archivos\\";
+            //Añadimos todas las direcciones a las que tendremos que hacer referencia, estas son, idiomas, una para cada idioma
+            //Logs, recursos, imagenes, usuario, credenciales, etc.
+            Directorios.Add("logs", DireccionBase + "\\Logs\\");
+            Directorios.Add("idiomas", DireccionBase + "\\Idiomas\\");
+            Directorios.Add("idioma_ingles", DireccionBase + "\\Idiomas\\EN\\");
+            Directorios.Add("idioma_espanol", DireccionBase + "\\Idiomas\\ES\\");
+            Directorios.Add("clave_encriptado", DireccionBase + "\\Utils\\Keys\\");
+            Directorios.Add("credenciales", DireccionBase + "\\Utils\\Credentials\\");
+            Directorios.Add("iconos", DireccionBase + "\\Imagenes\\Icons");
+            Directorios.Add("imagenes_defecto", DireccionBase + "\\Imagenes\\Default\\");
+            Directorios.Add("usuario", DireccionBase + "\\Usuarios\\");
+            Directorios.Add("imagenes_usuario", DireccionBase + "\\Usuarios\\Imagenes\\");
+            Directorios.Add("imagenes_usuario", DireccionBase + "\\Usuarios\\Archivos\\");
+            Directorios.Add("recursos", DireccionBase + "\\Resources\\");
 
-
+            //Nos aseguramos de que estan todas las carpetas importantes creadas, creandolas si faltan.
+            foreach (string direccion in Directorios.Values)
+            {
+                Directory.CreateDirectory(direccion);
+            }
         }
 
     }
